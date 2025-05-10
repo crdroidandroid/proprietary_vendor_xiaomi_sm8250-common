3de141c54edd1eec12e430d7f2fe2c2e7fe96581
johnmart19 <johnivan19999@gmail.com>
2025-05-10 13:27:02 +0300

    sm8250-common: Import QCOM implementation for audio effects
    - From HyperOS Alioth 1.0.3.0 Mi

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -252,6 +252,9 @@ PRODUCT_PACKAGES += \
     libasphere \
     libhwdap \
     libmisoundfx \
+    libqcbassboost \
+    libqcreverb \
+    libqcvirt \
     libshoebox \
     libswgamedap \
     libswvqe \
