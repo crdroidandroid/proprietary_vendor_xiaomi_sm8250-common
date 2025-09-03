167eff43fdac715fb826beec4c5cb4f0067e21c2
John Macka <macka2004@gmail.com>
2025-09-03 14:06:29 +0800

    sm8250-common: Import 32bit adsp/cdsp default listener libs
    In logs while boot:
    *sscrpcd : vendor/qcom/proprietary/sensors-see/sscrpcd/src/sscrpcd.cpp:86:adsp_default_listener_start called for [(null)]

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -156,8 +156,10 @@ PRODUCT_PACKAGES += \
     libOpenCL \
     libVkLayer_q3dtools \
     libadreno_utils \
+    libadsp_default_listener \
     libadsprpc \
     libc2d30_bltlib \
+    libcdsp_default_listener \
     libcdsprpc \
     libdiag \
     libfastcvdsp_stub \
@@ -320,7 +322,6 @@ PRODUCT_PACKAGES += \
     libQTEEConnector_vendor \
     libSecureUILib \
     libStDrvInt \
-    libadsp_default_listener \
     libaoa \
     libasn1cper \
     libasn1crt \
@@ -329,7 +330,6 @@ PRODUCT_PACKAGES += \
     libcacertclient \
     libcdfw \
     libcdfw_remote_api \
-    libcdsp_default_listener \
     libcdsprpc_system \
     libcne \
     libcneapiclient \
